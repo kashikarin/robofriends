20db87782d53feb949b084bc5033bfd6a6e0e550
Your Name <you@example.com>
2024-11-19 22:50:18 +0200

    Initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from "react";
+import Cardlist from '../components/Cardlist';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+
+
+class App extends Component {
+   constructor () {
+    super()
+    this.state = {
+        robots: [],
+        searchfield: ""
+   }
+}
+componentDidMount () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resp=>resp.json())
+    .then(users=>this.setState({robots: users}))
+}
+
+onSearchChange = (event) => {
+    this.setState ({searchfield: event.target.value});
+    
+}
+
+    render() {
+        const {robots, searchfield} = this.state;
+    const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchfield.toLowerCase()));
+    return !robots.length ?
+        <h1>Loading...</h1> :
+   
+            (<div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundry>
+                        <Cardlist robots={filteredRobots}/>
+                    </ErrorBoundry>
+                </Scroll>
+            </div>)
+    };
+   } 
+
+
+export default App;
